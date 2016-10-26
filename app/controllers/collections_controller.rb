19a9ff09933164541b3586134edd459c154383c4
Torri Porter <torri.porter@yahoo.com>
2016-10-26 12:18:04 -0700

    added variables to controllers

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,15 +1,54 @@
 class CollectionsController < ApplicationController
 
-  def index
+  def new
+    @collection = Collection.new
   end
 
-  def new
+  def create
+    @collection = Collection.new collection_params
+    if @collection.save
+      redirect_to collection_path(@collection)
+    else
+      flash[:alert] = "Collection not Created"
+      render :new
+    end
   end
 
   def show
+    @collection = Collection.find params[:id]
+    respond_to do |format|
+      format.html
+      format.json {render json: {collection: @collection}}
+    end
+  end
+
+  def index
+    @collection = Collection.order("name ASC")
   end
 
   def edit
   end
-  
+
+  def update
+    if @collection.update collection_params
+      redirect_to collection_path(@collection), notice: "Collection Changed"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @collection.destroy
+    redirect_to collections_path, notice: "Collection Deleted"
+  end
+
+  private
+
+  def collection_params
+    params.require(:collection).permit(:title, :year, :description)
+  end
+
+  def find_collection
+    @collection = Collection.find params[:id]
+  end
 end
--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,14 +1,54 @@
 class PaintingsController < ApplicationController
 
-  def index
+  def new
+    @painting = Painting.new
   end
 
-  def new
+  def create
+    @painting.Painting.new painting_params
+    if @painting.save
+      redirect_to painting_path(@painting)
+    else
+      flash[:alert] = "Painting not added"
+      render :new
+    end
   end
 
   def show
+    @painting = Painting.find params[:id]
+    respond_to do |format|
+      format.html
+      format.json {render json: {painting: @painting}}
+    end
+  end
+
+  def index
+    @paintings = Paintings.order("name ASC")
   end
 
   def edit
+  end
+
+  def update
+    if @painting.update painting_params
+      redirect_to painting_path(@painting), notice: "Painting Changed"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @painting.destroy
+    redirect_to paintings_path, notice: "Painting Deleted"
+  end
+
+  private
+
+  def painting_params
+    params.require(:painting).permit(:title, :year, :description, :dimentions)
+  end
+
+  def find_painting
+    @painting = Painting.find params[:id]
   end
 end
